264aba5a4ceb6e470907d6f477699baba0b02b2e
Dhruv <sdhruv1201@gmail.com>
2021-07-27 15:13:08 +0530

    inital commit

--- script.js
+++
@@ -1,6 +0,0 @@
-// jshint esversion: 6
-
-document.addEventListener("scroll", function (e) {
-  let nav = document.getElementsByClassName("nav")[0];
-  nav.classList.toggle('scrolled', window.scrollY > 0);
-});
