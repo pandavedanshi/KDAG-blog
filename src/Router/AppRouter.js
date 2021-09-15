87588712aaf22b97e9d097d0b5fc14fcd2c2876c
shubhraneel <shubhraneel@gmail.com>
2021-09-15 23:57:04 +0530

    Browser router -> Hash router

--- src/Router/AppRouter.js
+++ src/Router/AppRouter.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import LandingPage from "../components/Landing/LandingPage";
 import BlogPage from "../components/Blog/BlogPage";
 
 const AppRouter = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <LandingPage />
-        </Route>
-        <Route path="/blogs">
-          <BlogPage />
-        </Route>
-      </Switch>
-    </Router>
+    <React.StrictMode>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/blogs">
+            <BlogPage />
+          </Route>
+        </Switch>
+      </Router>
+    </React.StrictMode>
   );
 };
 
