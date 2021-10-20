fb29f3064241ec75124e16df15783b49a2765319
belongstodzire <abhilashxaviers@gmail.com>
2021-10-20 12:52:55 +0530

    sorting members

--- src/components/TeamPage/AdvisorsStatic.js
+++ src/components/TeamPage/AdvisorsStatic.js
@@ -3,44 +3,48 @@ import naruto  from "../../assets/pics/naruto.jpg";
 const members = [
     {
         image: naruto,
-        name: "Yash Agarwal",
-        email: "yash@gmail.com",
+        name: "Aditi Singhania",
         facebook: "www.facebook.com",
         twitter: "www.twitter.com",
-        linkedin: "www.linkedin.com"
+        linkedin: "https://www.linkedin.com/mwlite/in/aditi-singhania-436371146"
     },
     {
         image: naruto,
-        name: "Yash Agarwal",
-        email: "yash@gmail.com",
+        name: "Aditya Girish Pawate",
         facebook: "www.facebook.com",
         twitter: "www.twitter.com",
-        linkedin: "www.linkedin.com"
+        linkedin: "https://www.linkedin.com/in/aditya-girish-pawate-16a023169/"
     },
     {
         image: naruto,
-        name: "Yash Agarwal",
-        email: "yash@gmail.com",
+        name: "Onkar Sabnis",
         facebook: "www.facebook.com",
         twitter: "www.twitter.com",
-        linkedin: "www.linkedin.com"
+        linkedin: "https://www.linkedin.com/mwlite/in/onkar-sabnis-883094170"
     },
     {
         image: naruto,
-        name: "Yash Agarwal",
-        email: "yash@gmail.com",
+        name: "Siba Smarak Panigrahi",
         facebook: "www.facebook.com",
         twitter: "www.twitter.com",
-        linkedin: "www.linkedin.com"
+        linkedin: "https://www.linkedin.com/in/siba-smarak-panigrahi-42b38a213/"
     },
     {
         image: naruto,
-        name: "Yash Agarwal",
-        email: "yash@gmail.com",
+        name: "Srinivas Iyengar",
         facebook: "www.facebook.com",
         twitter: "www.twitter.com",
-        linkedin: "www.linkedin.com"
+        linkedin: "https://www.linkedin.com/mwlite/in/srinivas-iyengar-8a7777187"
+    },
+
+    {
+        image: naruto,
+        name: "Varun Madhavan",
+        facebook: "www.facebook.com",
+        twitter: "www.twitter.com",
+        linkedin: "https://www.linkedin.com/mwlite/in/varun-madhavan"
     }
+
 ]
 
 export default members;
--- src/components/TeamPage/MembersStatic.js
+++ src/components/TeamPage/MembersStatic.js
@@ -3,27 +3,20 @@ import naruto  from "../../assets/pics/naruto.jpg";
 const members = [
     {
         image: naruto,
-        name: "Yash Agarwal",
-        email: "yash@gmail.com",
-        facebook: "https://www.facebook.com/yash.agrawal.754570/",
-        twitter: "www.twitter.com",
-        linkedin: "https://www.linkedin.com/in/yash-agrawal-6b4253194/"
-    },
-    {
-        image: naruto,
-        name: "Ritik",
-        email: "yash@gmail.com",
-        facebook: "https://m.facebook.com/100026094961171/",
-        linkedin: "https://www.linkedin.com/in/the-ritik-singh/"
-    },
-    {
-        image: naruto,
         name: "Duhita Wani",
         email: "yash@gmail.com",
         facebook: "https://www.facebook.com/duhita.wani",
         twitter: "www.twitter.com",
         linkedin: "https://www.linkedin.com/in/duhita-wani-b4a393193/"
     },
+ {
+        image: naruto,
+        name: "Ritik",
+        email: "yash@gmail.com",
+        facebook: "https://m.facebook.com/100026094961171/",
+        linkedin: "https://www.linkedin.com/in/the-ritik-singh/"
+    },
+   
     {
         image: naruto,
         name: "Shivam Bhosale",
@@ -32,6 +25,7 @@ const members = [
         twitter: "www.twitter.com",
         linkedin: "https://www.linkedin.com/in/bhosaleshivam/"
     },
+
     {
         image: naruto,
         name: "Sohan Patnaik",
@@ -39,7 +33,17 @@ const members = [
         facebook: "https://www.facebook.com/sohan.patnaik.10",
         twitter: "www.twitter.com",
         linkedin: "https://www.linkedin.com/in/sohan-patnaik-29061a1a4/"
+    },
+
+    {
+        image: naruto,
+        name: "Yash Agarwal",
+        email: "yash@gmail.com",
+        facebook: "https://www.facebook.com/yash.agrawal.754570/",
+        twitter: "www.twitter.com",
+        linkedin: "https://www.linkedin.com/in/yash-agrawal-6b4253194/"
     }
+
 ]
 
 export default members;
