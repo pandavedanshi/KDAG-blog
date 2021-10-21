a78d19f79c741a21488f78d9afc5568e4044a3bd
ronakagarwal3434 <60756098+ronakagarwal3434@users.noreply.github.com>
2021-10-21 19:45:57 +0530

    Updated images links

--- src/components/TeamPage/SeniorMembersStatic.js
+++ src/components/TeamPage/SeniorMembersStatic.js
@@ -2,7 +2,7 @@ import naruto  from "../../assets/pics/naruto.jpg";
 
 const members = [
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=1Ms0k34H_oaFOh5upGdyegZg1Vy3jArAe",
         name: "Abhilash Sinha",
         // email: "abhilashxaviers@gmail.com",
         facebook: "www.facebook.com",
@@ -11,13 +11,13 @@ const members = [
 
     },
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=1Eh9MN0mSH6-q1_T0jSLtVPiFzIAlfmwP",
         name: "Abhranil Chandra",
         linkedin: "https://www.linkedin.com/in/abhranil-chandra-462332136/"
 
     },
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=1bhiiXkjKTNtT26rpwdQ3R8n8EsBkFn0Z",
         name: "Arun Kumar",
         email: "yash@gmail.com",
         facebook: "www.facebook.com",
@@ -27,7 +27,7 @@ const members = [
     },
 
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=138jNm9ml7rwfdXXdw5nJoh1GDbWE2Vga",
         name: "Ayush Surana",
         email: "yash@gmail.com",
         facebook: "www.facebook.com",
@@ -36,7 +36,7 @@ const members = [
 
     },
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=1mdMaeGvKi8Spt8bgkefSh_La1AvvNSBE",
         name: "Chinmay Jain",
         email: "yash@gmail.com",
         facebook: "www.facebook.com",
@@ -45,7 +45,7 @@ const members = [
 
     },
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=1mKQ0NxzefaX2jzPJ2-Yz0aV4EKCUkkNE",
         name: "Hari G",
         email: "yash@gmail.com",
         facebook: "www.facebook.com",
@@ -54,7 +54,7 @@ const members = [
 
     },
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=1kSCdUedBggZqC6BZNObvixjIgIJaJYZP",
         name: "Manav Nitin Kapadnis",
         email: "yash@gmail.com",
         facebook: "www.facebook.com",
@@ -63,19 +63,19 @@ const members = [
 
     },
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=1dfWyU6M4UuAw2l0nHkyCyemnPWlYa4Aj",
         name: "Manthan Patel",
         linkedin: "https://www.linkedin.com/in/manthan-patel-2655041a0"
 
     },
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=1OpfW8Ut-UAc-2q3B1L95sUTn0Ckvd3st",
         name: "Murali Manohar",
         linkedin: "https://www.linkedin.com/in/murali-manohar-bb848a1a7/"
 
     },
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=1FbEjwicg4y92lqQvWFQulN83iYFSBzpt",
         name: "Ratnesh Bhosale ",
         email: "yash@gmail.com",
         facebook: "www.facebook.com",
@@ -84,13 +84,13 @@ const members = [
 
     },
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=1BbFQmlP1T82QvE6kWtEQIamZzz0LMqA5",
         name: "Ronak Agarwal",
         linkedin:"https://www.linkedin.com/in/ronakagarwal3434"
 
     },
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=1CS4r0q6-cwMbkOaSSxsDJZrogFAp3e1t",
         name: "Roshni Kar",
         email: "yash@gmail.com",
         facebook: "www.facebook.com",
@@ -99,13 +99,13 @@ const members = [
 
     },
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=1m_fe1YraLI1ABJYRV4lanIu2VohpFtq4",
         name: "Shalini Bansal",
         linkedin: "https://www.linkedin.com/in/shalini-bansal-9314431b4/"
 
     },
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=19eLYiyy1chJTDcfNzKobG7W5TKU9UG7y",
         name: "Shubhraneel Pal",
         email: "yash@gmail.com",
         facebook: "www.facebook.com",
@@ -115,7 +115,7 @@ const members = [
     },
 
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=1BT-qLm-TKK7g-t4u1hMQmx7u7ZSCB4EV",
         name: "Shraman Pal",
         email: "yash@gmail.com",
         facebook: "www.facebook.com",
@@ -124,13 +124,13 @@ const members = [
 
     },
     {   
-        image: naruto,
+        image: "https://drive.google.com/uc?id=11YVs0uv2rpYdz6azfnver5myiEEgaQfS",
         name: "Siddharth Ranjan Bajpayi",
         linkedin: "https://www.linkedin.com/in/siddharth-ranjan-841689177"
 
     },
     {
-        image: naruto,
+        image: "https://drive.google.com/uc?id=1_1MJQmmG_eNjVnsOPnBy52zIpfnW6NLn",
         name: "Vaishnavi Malwade",
         email: "yash@gmail.com",
         facebook: "www.facebook.com",
