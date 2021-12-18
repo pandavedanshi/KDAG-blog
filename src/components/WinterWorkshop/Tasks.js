9c8a8ce5733d6575ce54675760b346778576adde
Shubhraneel Pal <shubhraneelpal@gmail.com>
2021-12-18 09:00:04 +0530

    .

--- src/components/WinterWorkshop/Tasks.js
+++ src/components/WinterWorkshop/Tasks.js
@@ -4,24 +4,30 @@ import TabHeading from "./TabHeading";
 const Tasks = (props) => {
   return (
     <div>
-    <TabHeading title="Tasks" />      
-      <div style={{marginTop: "1rem"}}>
-        {props.tasks.length > 0 ? props.tasks.map((e) => (
-          <p
-            style={{
-              fontSize: "1.2rem",
-              color: "#777",
-              padding: "1rem",
-              boxShadow: "1px 1px 10px 1px rgba(0, 0, 0, 0.2",
-              maxWidth: "30rem",
-              borderRadius: "10px",
-              marginLeft: "1rem",
-              marginBottom: "1rem"
-            }}
-          >
-            {e}
-          </p>
-        )) : "No Tasks"}
+      <TabHeading title="Tasks" />
+      <div style={{ marginTop: "1rem" }}>
+        {props.tasks.length > 0
+          ? props.tasks.map((e) => (
+              <a href={e.link} target="_blank" rel="noopener noreferrer">
+                <p
+                  style={{
+                    fontSize: "1.2rem",
+                    color: "#777",
+                    padding: "1rem",
+                    boxShadow: "1px 1px 10px 1px rgba(0, 0, 0, 0.2",
+                    maxWidth: "30rem",
+                    borderRadius: "10px",
+                    marginLeft: "1rem",
+                    marginBottom: "1rem",
+                    display: "flex",
+                    justifyContent: "space-between"
+                  }}
+                >
+                  {e.name} <i class="fas fa-external-link-alt"></i>
+                </p>
+              </a>
+            ))
+          : "No Tasks"}
       </div>
     </div>
   );
--- src/components/WinterWorkshop/data.js
+++ src/components/WinterWorkshop/data.js
@@ -11,11 +11,12 @@ const DataApi = [
         sessions: [
           {
             id: 1,
-            time: "10 Nov, 11:00am - 1:00pm",
+            time: "16 Dec, 11:00am - 1:00pm",
             recordinglink:
               "https://iitkgpacin.sharepoint.com/sites/KDAGPanelDiscussion/Shared%20Documents/General/Recordings/Winter%20Workshop%20%2721%20(Day%200)_20211216_053908.mp4?web=1",
             topics: ["Introduction", "Linear", "Logistic"],
-            presentation: "https://docs.google.com/presentation/d/1xaJmcKtsrEgqqNIe418ulstpqnhKBlaBMoRujH8cAu8/edit?hl=en_GB#slide=id.g107a52f84eb_2_598"
+            presentation:
+              "https://docs.google.com/presentation/d/1xaJmcKtsrEgqqNIe418ulstpqnhKBlaBMoRujH8cAu8/edit?hl=en_GB#slide=id.g107a52f84eb_2_598",
           },
         ],
       },
@@ -81,19 +82,22 @@ const DataApi = [
           {
             heading: "Logistic Regression",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
           {
             heading: "Linear Regression from scratch",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
           {
             heading: "Linear Regression from normal equation",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
         ],
@@ -102,14 +106,23 @@ const DataApi = [
         tab: 4,
         icon: "/icons/taskgray.svg",
         title: "Tasks",
-        tasks: [ ],
+        tasks: [
+          {
+            link: "https://forms.gle/udkTBVU6q5pG1Rea8",
+            name: "Linear Regression",
+          },
+          {
+            link: "https://docs.google.com/forms/d/e/1FAIpQLSfEqiqhbXdtvyp3AdbniwuoR_WJ5eZfoduf2WmVV9UTgk1oYQ/viewform?usp=sf_link",
+            name: "Logistic Regression",
+          },
+        ],
       },
     ],
   },
   {
     id: 1,
     day: "Day 1",
-    status: false,
+    status: true,
     category: [
       {
         tab: 1,
@@ -117,11 +130,18 @@ const DataApi = [
         sessions: [
           {
             id: 1,
-            time: "10 Nov, 11:00am - 1:00pm",
-            recordinglink:
-              "https://iitkgpacin.sharepoint.com/sites/KDAGPanelDiscussion/Shared%20Documents/General/Recordings/Winter%20Workshop%20%2721%20(Day%200)_20211216_053908.mp4?web=1",
-            topics: ["Introduction", "Linear", "Logistic"],
+            time: "17 Dec, 11:00am - 1:00pm",
+            recordinglink: "#",
+            topics: ["KNN", "Nearest Mean Classification"],
+            presentation: "https://docs.google.com/presentation/d/1T9mVY582yDRkzH_dF6Zg-9gbY2DmJ8Y0h2raLmxHhPc/edit#slide=id.g10860a216a7_0_63",
           },
+          {
+            id: 2,
+            time: "17 Dec, 11:00am - 1:00pm",
+            recordinglink: "#",
+            topics: ["SVM", "Kernels"],
+            presentation: "https://docs.google.com/presentation/d/1q7lJCQGWcSaaFwGBEGYXesQZ4JsXoVsGEbaoNrz5gA8/edit?usp=sharing",
+          }
         ],
       },
       {
@@ -130,51 +150,38 @@ const DataApi = [
         title: "Resources",
         blogs: [
           {
-            topic: "Linear Regression",
-            heading: "KDAG AI Weekly Blog on Linear Regression",
-            link: "https://kdagiit.medium.com/linear-regression-ba3fe4ba38c0",
+            topic: "KNN",
+            heading: "DataCamp - KNN Classification using Scikit-learn",
+            link: "https://www.datacamp.com/community/tutorials/k-nearest-neighbor-classification-scikit-learn",
           },
           {
-            topic: "Logistic Regression",
-            heading: "KDAG AI Weekly Blog on Logistic Regression",
-            link: "https://kdagiit.medium.com/logistic-regression-60194ba582e8",
+            topic: "SVM",
+            heading: "Nonlinear SVM",
+            link: "https://nlp.stanford.edu/IR-book/html/htmledition/nonlinear-svms-1.html",
           },
         ],
         videos: [
           {
-            topic: "Linear Regression",
-            heading: "Stanford Machine Learning course",
-            link: "https://www.youtube.com/watch?v=4b4MUYve_U8&list=PLoROMvodv4rMiGQp3WXShtMGgzqpfVfbU&index=2&t=1867s",
+            topic: "KNN",
+            heading:
+              "CS231n Winter 2016: Lecture 2: Data-driven approach, kNN, Linear Classification 1",
+            link: "https://www.youtube.com/watch?v=8inugqHkfvE",
           },
           {
-            topic: "Linear Regression",
-            heading: "Krish Naik Machine Learning Playlist",
-            link: "https://www.youtube.com/watch?v=1-OGRohmH2s&list=PLZoTAELRMXVPBTrWtJkn3wWQxZkmTXGwe&index=34",
+            topic: "SVM",
+            heading: "Lecture 6 - Support Vector Machines | Stanford CS229",
+            link: "https://www.youtube.com/watch?v=lDwow4aOrtg",
           },
           {
-            topic: "Linear Regression",
-            heading: "Krish Naik Machine Learning Playlist",
-            link: "https://www.youtube.com/watch?v=5rvnlZWzox8&list=PLZoTAELRMXVPBTrWtJkn3wWQxZkmTXGwe&index=37",
+            topic: "SVM",
+            heading: "Support Vector Machine (SVM) Basic Intuition- Part 1",
+            link: "https://youtu.be/H9yACitf-KM",
           },
           {
-            topic: "Logistic Regression",
-            heading: "Stanford Machine Learning course",
-            link: "https://www.youtube.com/watch?v=het9HFqo1TQ&list=PLoROMvodv4rMiGQp3WXShtMGgzqpfVfbU&index=3",
-          },
-          {
-            topic: "Logistic Regression",
-            heading: "Krish Naik Machine Learning Playlist",
-            link: "https://www.youtube.com/watch?v=L_xBe7MbPwk&list=PLZoTAELRMXVPBTrWtJkn3wWQxZkmTXGwe&index=47",
-          },
-          {
-            topic: "Logistic Regression",
-            heading: "Krish Naik Machine Learning Playlist",
-            link: "https://www.youtube.com/watch?v=uFfsSgQgerw&list=PLZoTAELRMXVPBTrWtJkn3wWQxZkmTXGwe&index=48",
-          },
-          {
-            topic: "Logistic Regression",
-            heading: "Krish Naik Machine Learning Playlist",
-            link: "https://www.youtube.com/watch?v=V8fS0T_ktn4&list=PLZoTAELRMXVPBTrWtJkn3wWQxZkmTXGwe&index=49",
+            topic: "SVM",
+            heading:
+              "Maths Intuition Behind Support Vector Machine Part 2 | Machine Learning Data Science",
+            link: "https://youtu.be/Js3GLb1xPhc",
           },
         ],
       },
@@ -184,21 +191,17 @@ const DataApi = [
         title: "Projects",
         projectlist: [
           {
-            heading: "Logistic Regression",
+            heading: "KNN",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://colab.research.google.com/drive/1OhyrRCNRq5xbo9BB9p-S67uPHTu_A1PJ?usp=sharing#scrollTo=IRZR78zY1dQU",
             description: "",
           },
           {
-            heading: "Linear Regression from scratch",
+            heading: "SVM",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
-            description: "",
-          },
-          {
-            heading: "Linear Regression from normal equation",
-            githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://colab.research.google.com/drive/1gyBewDZE759M9x35NZOA50tWFAUGM5CF?usp=sharing",
             description: "",
           },
         ],
@@ -207,7 +210,16 @@ const DataApi = [
         tab: 4,
         icon: "/icons/taskgray.svg",
         title: "Tasks",
-        tasks: [ ],
+        tasks: [
+          {
+            link: "https://docs.google.com/forms/d/1B8PqfoMgGUvxvEhG2lll_TqbCsQUSXNkaMOY7N5JzdQ/edit",
+            name: "KNN",
+          },
+          {
+            link: "https://forms.gle/6LUh8F8txoS95tpe9",
+            name: "SVM",
+          }
+        ],
       },
     ],
   },
@@ -291,19 +303,22 @@ const DataApi = [
           {
             heading: "Logistic Regression",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
           {
             heading: "Linear Regression from scratch",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
           {
             heading: "Linear Regression from normal equation",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
         ],
@@ -312,7 +327,7 @@ const DataApi = [
         tab: 4,
         icon: "/icons/taskgray.svg",
         title: "Tasks",
-        tasks: [ ],
+        tasks: [],
       },
     ],
   },
@@ -396,19 +411,22 @@ const DataApi = [
           {
             heading: "Logistic Regression",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
           {
             heading: "Linear Regression from scratch",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
           {
             heading: "Linear Regression from normal equation",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
         ],
@@ -417,7 +435,7 @@ const DataApi = [
         tab: 4,
         icon: "/icons/taskgray.svg",
         title: "Tasks",
-        tasks: [ ],
+        tasks: [],
       },
     ],
   },
@@ -501,19 +519,22 @@ const DataApi = [
           {
             heading: "Logistic Regression",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
           {
             heading: "Linear Regression from scratch",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
           {
             heading: "Linear Regression from normal equation",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
         ],
@@ -522,7 +543,7 @@ const DataApi = [
         tab: 4,
         icon: "/icons/taskgray.svg",
         title: "Tasks",
-        tasks: [ ],
+        tasks: [],
       },
     ],
   },
@@ -606,19 +627,22 @@ const DataApi = [
           {
             heading: "Logistic Regression",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
           {
             heading: "Linear Regression from scratch",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
           {
             heading: "Linear Regression from normal equation",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
         ],
@@ -627,7 +651,7 @@ const DataApi = [
         tab: 4,
         icon: "/icons/taskgray.svg",
         title: "Tasks",
-        tasks: [ ],
+        tasks: [],
       },
     ],
   },
@@ -711,19 +735,22 @@ const DataApi = [
           {
             heading: "Logistic Regression",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
           {
             heading: "Linear Regression from scratch",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
           {
             heading: "Linear Regression from normal equation",
             githublink: "",
-            drivelink: "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
+            drivelink:
+              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
             description: "",
           },
         ],
@@ -732,10 +759,10 @@ const DataApi = [
         tab: 4,
         icon: "/icons/taskgray.svg",
         title: "Tasks",
-        tasks: [ ],
+        tasks: [],
       },
     ],
-  }
-]
+  },
+];
 
 export default DataApi;
