096b6b126a40d22e822e9b2b87290ee81990f74b
Chaitanya Dhuware <84279110+ChaitnayaD@users.noreply.github.com>
2021-12-20 23:03:21 +0530

    Update data.js

--- src/components/WinterWorkshop/data.js
+++ src/components/WinterWorkshop/data.js
@@ -224,8 +224,8 @@ const DataApi = [
     ],
   },
   {
-    id: 2,
-    day: "Day 2",
+    id: 3,
+    day: "Day 3",
     status: true,
     category: [
       {
@@ -234,10 +234,10 @@ const DataApi = [
         sessions: [
           {
             id: 1,
-            time: "18 Dec, 11:00am - 1:00pm",
+            time: "20 Dec, 11:00am - 1:00pm",
             recordinglink: "https://drive.google.com/file/d/1ABqJPc4dg0B4Sg93oOCVEtHFnOyGNSWy/view?usp=sharing",
-            topics: ["Bayesian Classifier", "Decision Trees"],
-            presentation: "https://docs.google.com/presentation/d/1-uZkfUKSdblLttyRGWO-rCuEr16TOjfLQiq7AisW8GI/edit#slide=id.g10879f6aa51_0_215"
+            topics: ["Bagging", "Random Forest"],
+            presentation: "https://docs.google.com/presentation/d/10tMNKvsCC2llg1UI0o4Irh4ROmBs2vkEUOqYL5aqX5M/edit?usp=sharing"
           },
         ],
       },
@@ -247,51 +247,36 @@ const DataApi = [
         title: "Resources",
         blogs: [
           {
-            topic: "Decision Trees",
-            heading: "Datacamp Decision Trees",
-            link: "https://www.datacamp.com/community/tutorials/decision-tree-classification-python",
+            topic: "Random Forest",
+            heading: "Datacamp Random Forest",
+            link: "https://www.datacamp.com/community/tutorials/random-forests-classifier-python",
           },
           {
-            topic: "Decision Trees",
-            heading: "KDAG AI Weekly Blog on Decision Trees",
-            link: "https://kdagiit.medium.com/decision-trees-just-the-everyday-things-d41056e77ff2",
+            topic: "Random Forest & Bagging",
+            heading: "KDAG AI Weekly Blog on Random Forest and Bagging",
+            link: "https://kdagiit.medium.com/bagging-and-random-forests-reducing-bias-and-variance-using-randomness-8d516214fe7f",
           },
           {
-            topic: "Bayesian Classifiers",
-            heading: "KDAG AI Weekly Blog on Bayesian Classifier",
-            link: "https://kdagiit.medium.com/naive-bayes-algorithm-4b8b990c7319",
+            topic: "Bagging & Boosting",
+            heading: "Bagging and Boosting by KDAG advisor SSP",
+            link: "https://www.youtube.com/playlist?list=PLcvdI4f2qdKWnxiaUuOb6Jffwbsfs5jM3",
+          },
+          {
+            topic: "Bagging",
+            heading: "Datacamp Bagging Explanation",
+            link: "https://campus.datacamp.com/courses/machine-learning-with-tree-based-models-in-python/bagging-and-random-forests?ex=1",
           },
         ],
         videos: [
           {
-            topic: "Decision Trees",
-            heading: "Stanford Machine Learning course",
-            link: "https://www.youtube.com/watch?v=wr9gUr-eWdA&list=PLoROMvodv4rMiGQp3WXShtMGgzqpfVfbU&index=10",
+            topic: "Bagging",
+            heading: "Cornell Machine Learning course",
+            link: "https://www.youtube.com/watch?v=4EOCQJgqAOY",
           },
           {
-            topic: "Decision Trees",
-            heading: "How to avoid overfitting",
-            link: "https://www.youtube.com/watch?v=SLOyyFHbiqo",
-          },
-          {
-            topic: "Bayesian Classifiers",
-            heading: "Naive Bayes Classifier in Python | Edureka",
-            link: "https://www.youtube.com/watch?v=vz_xuxYS2PM",
-          },
-          {
-            topic: "Bayesian Classifiers",
-            heading: "Estimating Probabilities from Data: Naive Bayes - Cornell CS4780",
-            link: "https://www.youtube.com/watch?v=pDHEX2usCS0",
-          },
-          {
-            topic: "Bayesian Classifiers",
-            heading: "Naive Bayes, Clearly Explained",
-            link: "https://youtu.be/O2L2Uv9pdDA",
-          },
-          {
-            topic: "Bayesian Classifiers",
-            heading: "Gaussian Naive Bayes, Clearly Explained",
-            link: "https://youtu.be/H3EjCKtlVog",
+            topic: "Bagging",
+            heading: "Krish Naik Bagging Tutorial",
+            link: "https://www.youtube.com/watch?v=KIOeZ5cFZ50",
           },
         ],
       },
@@ -301,10 +286,10 @@ const DataApi = [
         title: "Projects",
         projectlist: [
           {
-            heading: "Bayesian Classifiers",
+            heading: "Bagging Classifier",
             githublink: "",
             drivelink:
-              "https://drive.google.com/drive/folders/1uUd3MbNyMkDoHHRf5oGAQ_y-zStWbd73?usp=sharing",
+              "https://colab.research.google.com/drive/1OVvgBJRdSK9V-UGNPkDZSg0MQJlIV8Ng?usp=sharing",
             description: "",
           }
         ],
@@ -315,119 +300,10 @@ const DataApi = [
         title: "Tasks",
         tasks: [
             {
-              link: "https://forms.gle/EpNmniHDpjyDTPcTA",
-              name: "Bayesian",
+              link: "https://docs.google.com/forms/d/e/1FAIpQLSfG_3Qh-sxyT6sdyGQTdEXOMSeHDnBbX0LBHA4G-JS1grCqew/viewform",
+              name: "Bagging and Random Forest",
             },
           ],
-      },
-    ],
-  },
-  {
-    id: 3,
-    day: "Day 3",
-    status: false,
-    category: [
-      {
-        tab: 1,
-        title: "Topics",
-        sessions: [
-          {
-            id: 1,
-            time: "10 Nov, 11:00am - 1:00pm",
-            recordinglink:
-              "https://iitkgpacin.sharepoint.com/sites/KDAGPanelDiscussion/Shared%20Documents/General/Recordings/Winter%20Workshop%20%2721%20(Day%200)_20211216_053908.mp4?web=1",
-            topics: ["Introduction", "Linear", "Logistic"],
-            presentation: ""
-          },
-        ],
-      },
-      {
-        tab: 2,
-        icon: "/icons/resourcesgray.svg",
-        title: "Resources",
-        blogs: [
-          {
-            topic: "Linear Regression",
-            heading: "KDAG AI Weekly Blog on Linear Regression",
-            link: "https://kdagiit.medium.com/linear-regression-ba3fe4ba38c0",
-          },
-          {
-            topic: "Logistic Regression",
-            heading: "KDAG AI Weekly Blog on Logistic Regression",
-            link: "https://kdagiit.medium.com/logistic-regression-60194ba582e8",
-          },
-        ],
-        videos: [
-          {
-            topic: "Linear Regression",
-            heading: "Stanford Machine Learning course",
-            link: "https://www.youtube.com/watch?v=4b4MUYve_U8&list=PLoROMvodv4rMiGQp3WXShtMGgzqpfVfbU&index=2&t=1867s",
-          },
-          {
-            topic: "Linear Regression",
-            heading: "Krish Naik Machine Learning Playlist",
-            link: "https://www.youtube.com/watch?v=1-OGRohmH2s&list=PLZoTAELRMXVPBTrWtJkn3wWQxZkmTXGwe&index=34",
-          },
-          {
-            topic: "Linear Regression",
-            heading: "Krish Naik Machine Learning Playlist",
-            link: "https://www.youtube.com/watch?v=5rvnlZWzox8&list=PLZoTAELRMXVPBTrWtJkn3wWQxZkmTXGwe&index=37",
-          },
-          {
-            topic: "Logistic Regression",
-            heading: "Stanford Machine Learning course",
-            link: "https://www.youtube.com/watch?v=het9HFqo1TQ&list=PLoROMvodv4rMiGQp3WXShtMGgzqpfVfbU&index=3",
-          },
-          {
-            topic: "Logistic Regression",
-            heading: "Krish Naik Machine Learning Playlist",
-            link: "https://www.youtube.com/watch?v=L_xBe7MbPwk&list=PLZoTAELRMXVPBTrWtJkn3wWQxZkmTXGwe&index=47",
-          },
-          {
-            topic: "Logistic Regression",
-            heading: "Krish Naik Machine Learning Playlist",
-            link: "https://www.youtube.com/watch?v=uFfsSgQgerw&list=PLZoTAELRMXVPBTrWtJkn3wWQxZkmTXGwe&index=48",
-          },
-          {
-            topic: "Logistic Regression",
-            heading: "Krish Naik Machine Learning Playlist",
-            link: "https://www.youtube.com/watch?v=V8fS0T_ktn4&list=PLZoTAELRMXVPBTrWtJkn3wWQxZkmTXGwe&index=49",
-          },
-        ],
-      },
-      {
-        tab: 3,
-        icon: "/icons/projectgray.svg",
-        title: "Projects",
-        projectlist: [
-          {
-            heading: "Logistic Regression",
-            githublink: "",
-            drivelink:
-              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
-            description: "",
-          },
-          {
-            heading: "Linear Regression from scratch",
-            githublink: "",
-            drivelink:
-              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
-            description: "",
-          },
-          {
-            heading: "Linear Regression from normal equation",
-            githublink: "",
-            drivelink:
-              "https://drive.google.com/drive/folders/1QCSw9TKCcw_Nsq43hGX_tEz917spPv1a",
-            description: "",
-          },
-        ],
-      },
-      {
-        tab: 4,
-        icon: "/icons/taskgray.svg",
-        title: "Tasks",
-        tasks: [],
       },
     ],
   },
