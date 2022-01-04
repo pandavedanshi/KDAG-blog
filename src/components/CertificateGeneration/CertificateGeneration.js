2543aeae41c2f7c19d0f3d7344fee2eb1744d06b
ChaitanyaD <crdhuware07@gmail.com>
2022-01-04 22:55:17 +0530

    Certificate Searchbar

--- src/components/CertificateGeneration/CertificateGeneration.js
+++ src/components/CertificateGeneration/CertificateGeneration.js
@@ -7,41 +7,44 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const CertificateGeneration = () => {
-    const [email, setEmail] = useState("");
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const found = certificates.find((element) => element.Gmail === email);
-        if (found) {
-            window.open(found["Certificate Link"]);
-            setEmail("");
-        } else {
-            toast.error("Enter registered email!", {
-                position: "top-center",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-            });
-        }
-    };
+  const [email, setEmail] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const found = certificates.find((element) => element.Gmail === email);
+    if (found) {
+      window.open(found["Certificate Link"]);
+      setEmail("");
+    } else {
+      toast.error("Enter registered email!", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
 
-    return (
-        <div>
-            <Navbar />
-            <Header />
-            <ToastContainer />
-            <form class="content" onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    placeholder="Enter your registered email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <ToastContainer />
+      <div className="search">
+        <form className="content search-form" onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Enter your registered email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default CertificateGeneration;
