fe9b62102e709ed8b3f8fedcbdc3dcd61423130b
Aritra Chakraborty <aritracodm2002@kgpian.iitkgp.ac.in>
2023-04-09 18:20:22 +0530

    Added python workshop

--- src/components/Resources/Resources/ResourcesStatic.js
+++ src/components/Resources/Resources/ResourcesStatic.js
@@ -1,33 +1,40 @@
 const resources = [
-    {
-      id: 1,
-      name: "Paper Reading Sessions",
-      subtitle: "Feb 2021 - Present",
-      description: "Come on data enthusiasts! How about an R-Rated session on Convolutional Neural Networks or a rather mathematical approach to understanding a deep learning model through research papers? The reading sessions are usually kept short and to the point. So let’s delve deep into exciting data science topics with proper resources!",
-      link: "https://docs.google.com/document/d/1nrZ4aYcKM5M9OT_cLahFPo2C2baajt6uRMB37ypfezY/edit#heading=h.u63rmwevtk85" 
-    },
-    {
-      id: 2,
-      name: "Panel Discussions",
-      subtitle: "March 2021",
-      description: "Wondering what to do for your CDC intern or how to apply for research opportunities in ML/AI? We have with us the pre-final year students who have cracked their CDC internships and panelists who have done research interns at various institutes in India and abroad. They are here to help with your questions such as what and how to prepare.",
-      link: "https://docs.google.com/document/d/1T0ERSENlLyKsLzqSzBtFuqvlsWumP4-xVS7z-uGm9X0/edit#heading=h.c69b3bdgsox"
-      
-    },
-    {
-      id: 3,
-      name: "Python Workshop",
-      subtitle: "Feb 2021",
-      description: "Why is Python so special to scientists? Python provides us with an easy-to-code, object-oriented high-level language. There are different libraries available for different fields like Math, Data Mining, Data exploration, and visualization. The workshop gives an introduction to Python programming and some useful Python libraries - NumPy, pandas, matplotlib.",
-      link: "https://docs.google.com/document/d/1QsdPACSAxuWnUuiuUmHOJ-oPh0dQDN_21DopcFy0tzQ/edit" 
-    },
-    {
-      id: 4,
-      name: "Winter Workshop",
-      subtitle: "Dec 2020",
-      description: "Explore Machine learning, Deep learning, and Python programming. Learn about basic visualization and machine learning techniques. The workshop covers visualization techniques, an introduction to machine learning algorithms, neural networks, unsupervised learning algorithms, time series, and an introduction to analyzing data through Kaggle.",
-      link: "https://docs.google.com/document/d/1iywV1LAs3cGmAn_xH0FgGRV3zdpzsXjbR6jewkl1cWw/edit" 
-    }
-  ]
+  {
+  id: 1,
+  name: "Python Workshop",
+  subtitle: "April 2023",
+  description: "Why is Python so special to scientists? Python provides us with an easy-to-code, object-oriented high-level language. There are different libraries available for different fields like Math, Data Mining, Data exploration, and visualization. The workshop gives an introduction to Python programming and some useful Python libraries - NumPy, pandas, matplotlib.",
+  link: "https://drive.google.com/drive/folders/1iQgB8yL8Pg42AsJ-yyJCFftQKy-KL1en?usp=share_link" 
+  },
+  {
+    id: 2,
+    name: "Paper Reading Sessions",
+    subtitle: "Feb 2021 - Present",
+    description: "Come on data enthusiasts! How about an R-Rated session on Convolutional Neural Networks or a rather mathematical approach to understanding a deep learning model through research papers? The reading sessions are usually kept short and to the point. So letâ€™s delve deep into exciting data science topics with proper resources!",
+    link: "https://docs.google.com/document/d/1nrZ4aYcKM5M9OT_cLahFPo2C2baajt6uRMB37ypfezY/edit#heading=h.u63rmwevtk85" 
+  },
+  {
+    id: 3,
+    name: "Panel Discussions",
+    subtitle: "March 2021",
+    description: "Wondering what to do for your CDC intern or how to apply for research opportunities in ML/AI? We have with us the pre-final year students who have cracked their CDC internships and panelists who have done research interns at various institutes in India and abroad. They are here to help with your questions such as what and how to prepare.",
+    link: "https://docs.google.com/document/d/1T0ERSENlLyKsLzqSzBtFuqvlsWumP4-xVS7z-uGm9X0/edit#heading=h.c69b3bdgsox"
+    
+  },
+  {
+    id: 4,
+    name: "Python Workshop",
+    subtitle: "Feb 2021",
+    description: "Why is Python so special to scientists? Python provides us with an easy-to-code, object-oriented high-level language. There are different libraries available for different fields like Math, Data Mining, Data exploration, and visualization. The workshop gives an introduction to Python programming and some useful Python libraries - NumPy, pandas, matplotlib.",
+    link: "https://docs.google.com/document/d/1QsdPACSAxuWnUuiuUmHOJ-oPh0dQDN_21DopcFy0tzQ/edit" 
+  },
+  {
+    id: 5,
+    name: "Winter Workshop",
+    subtitle: "Dec 2020",
+    description: "Explore Machine learning, Deep learning, and Python programming. Learn about basic visualization and machine learning techniques. The workshop covers visualization techniques, an introduction to machine learning algorithms, neural networks, unsupervised learning algorithms, time series, and an introduction to analyzing data through Kaggle.",
+    link: "https://docs.google.com/document/d/1iywV1LAs3cGmAn_xH0FgGRV3zdpzsXjbR6jewkl1cWw/edit" 
+  }
+]
 
 export default resources;
