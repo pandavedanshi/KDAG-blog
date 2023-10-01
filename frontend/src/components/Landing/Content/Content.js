da0b7bb7a7657155db57278b1001d98b5b168f85
RaajAakarsh <aakarshraaj032@gmail.com>
2023-10-02 00:12:37 +0530

    Css chnages made in the landing page and to hover timings

--- frontend/src/components/Landing/Content/Content.js
+++ frontend/src/components/Landing/Content/Content.js
@@ -16,8 +16,9 @@ const Content = () => {
           <Fade left>
           <div className="content-pair-text">
             <div className="content-pair-heading">
-              <span className="content-pair-word">BLOG</span>: Welcome to the pathway to master Data Science!
+              <span className="content-pair-word">BLOG</span> : Welcome to the pathway to master Data Science!
             </div>
+            <hr />
             <div className="content-pair-paragraph">
             As we dive deeper into the world of Machine Learning everyday, it becomes imperative to stay up-to-date with the different machine learning algorithms that not only help us build our data models but also provide an in-depth understanding of data science. Plunge right in and happy learning!
             </div>
@@ -37,8 +38,9 @@ const Content = () => {
 
         <div className="content-pair-text content-mobile">
             <div className="content-pair-heading">
-              <span className="content-pair-word">RESOURCES</span>: Choose your own path in machine learning by exploring our resource library!
+              <span className="content-pair-word">RESOURCES</span> : Choose your own path in machine learning by exploring our resource library!
             </div>
+            <hr />
             <div className="content-pair-paragraph">
             Every year KDAG conducts the Winter workshop for students of IIT KGP who are interested in the field of data analytics. From python basics to CNN, these modules have been meticulously curated to cover a spectrum of topics. KDAG also provides a planned preparation guide for interviews to help students with internship and placement drive
             </div>
@@ -57,8 +59,9 @@ const Content = () => {
         <Fade right>
           <div className="content-pair-text content-nonmobile">
             <div className="content-pair-heading content-nonmobile">
-              <span className="content-pair-word">RESOURCES</span>: Choose your own path in machine learning by exploring our resource library!
+              <span className="content-pair-word">RESOURCES</span> : Choose your own path in machine learning by exploring our resource library!
             </div>
+            <hr />
             <div className="content-pair-paragraph content-nonmobile">
             Every year KDAG conducts the Winter workshop for students of IIT KGP who are interested in the field of data analytics. From python basics to CNN, these modules have been meticulously curated to cover a spectrum of topics. KDAG also provides a planned preparation guide for interviews to help students with internship and placement drive
             </div>
@@ -74,6 +77,7 @@ const Content = () => {
             <div className="content-pair-heading">
               <span className="content-pair-word">EVENTS</span>: Check out the plethora of events conducted by KDAG!
             </div>
+            <hr />
             <div className="content-pair-paragraph">
             Data Science workshops, internship bootcamps, panel discussions on research internships opportunities in ML/AI , reading sessions , hackathons and what not! KDAG is pleased to bring it  forth to you to enhance your knowledge.You are just one click away from getting access to all of this! Go ahead and all the best!
             </div>
